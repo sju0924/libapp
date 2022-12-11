3e15c1ceec5ade2971c3128fa8960ed19732b715
jeon sohn <fe91990@gmail.com>
2022-12-12 02:20:10 +0900

    Update linking with backend server

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -5,22 +5,20 @@
   >
     <v-row>
       <v-col cols="12">
-         <v-radio-group
-              v-model="row"
-              row
-            >
-              <v-radio   v-for="feat in feats"
-                label="feat"
-                value="feat"
-                :key="feat"
+        <v-radio-group row>
+              <v-radio
+                v-for="n in feats"
+                :key="`${n}`"
+                :label="`${n}`"
+                :value="`${n}`"
+                v-on:click="selectFeat(`${n}`)"
               ></v-radio>
             </v-radio-group>
       </v-col>
       <v-col cols="10">
          <v-text-field
-                    v-model="message2"
                     solo
-                    label="Author"
+                    :label="getFeat"
                     clearable
                   >
                   </v-text-field>
@@ -50,113 +48,51 @@
 <script>
 export default {
     name: 'SearchTab',
-      data () {
+    mounted(){
+        const HOST = "http://localhost:8080";
+        this.$axios
+              .get(HOST + "/test").then((res)=>{
+              console.log(res.data);
+              this.desserts = res.data;
+              })
+
+
+    },
+     data () {
+
+
           return {
+           row: null,
+           selectedFeat: 'Author',
             headers: [
               {
-                text: 'Dessert (100g serving)',
+                text: 'Book list',
                 align: 'start',
                 sortable: false,
                 value: 'name',
               },
-              { text: 'Calories', value: 'calories' },
-              { text: 'Fat (g)', value: 'fat' },
-              { text: 'Carbs (g)', value: 'carbs' },
-              { text: 'Protein (g)', value: 'protein' },
-              { text: 'Iron (%)', value: 'iron' },
+              { text: 'title', value: 'title' },
+              { text: 'author', value: 'author' },
+              { text: 'year', value: 'year' },
+              { text: 'ISBN', value: 'isbn' },
             ],
             feats:['Author','Title', 'Year'],
-            desserts: [
-              {
-                name: 'Frozen Yogurt',
-                calories: 159,
-                fat: 6.0,
-                carbs: 24,
-                protein: 4.0,
-                iron: '1%',
-              },
-              {
-                name: 'Ice cream sandwich',
-                calories: 237,
-                fat: 9.0,
-                carbs: 37,
-                protein: 4.3,
-                iron: '1%',
-              },
-              {
-                name: 'Eclair',
-                calories: 262,
-                fat: 16.0,
-                carbs: 23,
-                protein: 6.0,
-                iron: '7%',
-              },
-              {
-                name: 'Cupcake',
-                calories: 305,
-                fat: 3.7,
-                carbs: 67,
-                protein: 4.3,
-                iron: '8%',
-              },
-              {
-                name: 'Gingerbread',
-                calories: 356,
-                fat: 16.0,
-                carbs: 49,
-                protein: 3.9,
-                iron: '16%',
-              },
-              {
-                name: 'Jelly bean',
-                calories: 375,
-                fat: 0.0,
-                carbs: 94,
-                protein: 0.0,
-                iron: '0%',
-              },
-              {
-                name: 'Lollipop',
-                calories: 392,
-                fat: 0.2,
-                carbs: 98,
-                protein: 0,
-                iron: '2%',
-              },
-              {
-                name: 'Honeycomb',
-                calories: 408,
-                fat: 3.2,
-                carbs: 87,
-                protein: 6.5,
-                iron: '45%',
-              },
-              {
-                name: 'Donut',
-                calories: 452,
-                fat: 25.0,
-                carbs: 51,
-                protein: 4.9,
-                iron: '22%',
-              },
-              {
-                name: 'KitKat',
-                calories: 518,
-                fat: 26.0,
-                carbs: 65,
-                protein: 7,
-                iron: '6%',
-              },
-            ],
-
-
-          }
-
-       },
-    methods:{
+            desserts: []
+        }
     },
+    methods:{
+        selectFeat (param) {
+             this.selectedFeat = param;
+        },
+    },
+    computed: {
+          getFeat () {
+             return this.selectedFeat;
+          }
+        },
+
     props: {
         msg: String
-    }
+    },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 // vuetify
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import axios from 'axios'
 
 
 import App from './App.vue'
 
-
+Vue.prototype.$eventBus = new Vue();
+Vue.prototype.$axios = axios
 Vue.use(Vuetify);
 
 Vue.config.productionTip = false
